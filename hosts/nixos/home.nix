91404847f131453a53629e500f34f0dd4608060c
Victor R <39545521+viicslen@users.noreply.github.com>
2024-05-09 09:25:11 -0400

    Remove unused sessionPath configuration in home.nix

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -9,9 +9,6 @@
 }: {
   imports = [] ++ lib.attrsets.mapAttrsToList (name: value: value) outputs.homeManagerModules;
 
-  # Set the user session paths
-  home.sessionPath = [];
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. If you don't want to manage your shell through Home
   # Manager then you have to manually source 'hm-session-vars.sh' located at
