a779d81e8bcf0a17158eca5640eccbd7752861d6
Victor R <39545521+viicslen@users.noreply.github.com>
2024-05-10 12:06:23 -0400

    chore: Comment out nix.settings in walker.nix

--- modules/nixos/hyprland/walker.nix
+++ modules/nixos/hyprland/walker.nix
@@ -3,10 +3,10 @@
     inputs.walker.homeManagerModules.walker
   ];
 
-  nix.settings = {
-    substituters = ["https://walker.cachix.org"];
-    trusted-public-keys = ["walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="];
-  };
+  # nix.settings = {
+  #   substituters = ["https://walker.cachix.org"];
+  #   trusted-public-keys = ["walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="];
+  # };
 
   programs.walker = {
     enable = true;
