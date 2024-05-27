bd03523b0e1ddf1dd07be4e167b6d773d630b03e
Victor R <39545521+viicslen@users.noreply.github.com>
2024-05-27 12:06:08 -0400

    Updated config files

--- modules/nixos/hyprland/default.nix
+++ modules/nixos/hyprland/default.nix
@@ -42,7 +42,6 @@ in {
       xdg.portal = {
         enable = true;
         wlr.enable = true;
-        extraPortals = [pkgs.xdg-desktop-portal-gtk];
       };
 
       security.pam.services.gdm.enableGnomeKeyring = true;
