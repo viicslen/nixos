cd9711eb9b2e0bfeece529888824937f52e32ed9
Victor R <39545521+viicslen@users.noreply.github.com>
2024-06-25 10:20:59 -0400

    Updated config files

--- base/nixos/home.nix
+++ base/nixos/home.nix
@@ -9,34 +9,43 @@
 }: {
   imports = [] ++ lib.attrsets.mapAttrsToList (name: value: value) outputs.homeManagerModules;
 
-  home.stateVersion = "24.05";
-  home.username = user;
-  home.homeDirectory = "/home/${user}";
-
-  programs.home-manager.enable = true;
-
   systemd.user.startServices = "sd-switch";
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    NIXOS_OZONE_WL = "1";
+  home = {
+    username = user;
+    homeDirectory = "/home/${user}";
+    stateVersion = "24.05";
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      NIXOS_OZONE_WL = "1";
+    };
   };
 
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-    tmux.enableShellIntegration = true;
-  };
+  programs = {
+    home-manager.enable = true;
 
-  programs.tmux = {
-    enable = true;
-    shortcut = "Space";
-    mouse = true;
-    baseIndex = 1;
-    keyMode = "vi";
-    historyLimit = 10000;
-    tmuxinator.enable = true;
-    shell = "${pkgs.zsh}/bin/zsh";
-    extraConfig = "source-file ~/.tmux.conf";
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      tmux.enableShellIntegration = true;
+    };
+
+    tmux = {
+      enable = true;
+      shortcut = "Space";
+      mouse = true;
+      baseIndex = 1;
+      keyMode = "vi";
+      historyLimit = 10000;
+      tmuxinator.enable = true;
+      shell = "${pkgs.zsh}/bin/zsh";
+      extraConfig = "source-file ~/.tmux.conf";
+    };
+
+    hstr = {
+      enable = true;
+      enableZshIntegration = true;
+    };
   };
 }
