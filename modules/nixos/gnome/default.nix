5817867856c21c720fafeea9ccf2cc8e52323017
Victor R <39545521+viicslen@users.noreply.github.com>
2024-08-26 09:36:45 -0400

    Updated config files

--- modules/nixos/gnome/default.nix
+++ modules/nixos/gnome/default.nix
@@ -115,6 +115,50 @@ in {
             disable-user-extensions = false;
             enabled-extensions = lists.forEach cfg.extensions (x: x.extensionUuid);
           };
+
+          # Extensions
+          "org/gnome/shell/extensions/lennart-k/rounded_corners" = {
+            corner-radius = 7;
+          };
+
+          "org/gnome/shell/extensions/astra-monitor" = {
+            network-header-show = false;
+            storage-header-show = false;
+          };
+
+          "org/gnome/shell/extensions/blur-my-shell/applications" = {
+            blur = true;
+            dynamic-opacity = false;
+            whitelist = [ "kitty" ];
+          };
+
+          "org/gnome/shell/extensions/dash-to-panel" = {
+            dot-color-dominant = true;
+            dot-color-override = false;
+            focus-highlight-dominant = true;
+            hide-overview-on-startup = true;
+            isolate-monitors = true;
+            isolate-workspaces = true;
+            overview-click-to-exit = true;
+            panel-element-positions = ''
+              {"0":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}],"1":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}
+            '';
+            trans-use-custom-opacity = true;
+            trans-use-dynamic-opacity = true;
+          };
+
+          "org/gnome/shell/extensions/arcmenu" = {
+            arcmenu-hotkey = [];
+            button-padding = 10;
+            custom-menu-button-icon-size = 30.0;
+            distro-icon = 22;
+            menu-button-appearance = "Icon";
+            menu-button-border-color = mkTuple [ true "transparent" ];
+            menu-button-border-radius = mkTuple [ true 10 ];
+            menu-button-icon = "Distro_Icon";
+            multi-monitor = true;
+            runner-hotkey = [ "<Control>Super_L" ];
+          };
         };
       };
     })
