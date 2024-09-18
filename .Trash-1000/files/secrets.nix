c1d55bddaceda94d56be459551cf8c3bca46b5ac
Victor R <39545521+viicslen@users.noreply.github.com>
2024-09-18 13:24:47 -0400

    chore: Remove obsolete age-encrypted files and related configurations

--- .Trash-1000/files/secrets.nix
+++
@@ -1,7 +0,0 @@
-let
-  sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVQJb4vtkKXIrgE440ywBqMLNKZvbLEbT7G5WEFIvL+";
-in
-{
-  "env.age".publicKeys = [ sshKey ];
-  "password.age".publicKeys = [ sshKey ];
-}
