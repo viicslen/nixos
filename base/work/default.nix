30efce581582957017cc684ccc1216aa26fafb38
Victor R <39545521+viicslen@users.noreply.github.com>
2024-09-19 12:16:37 -0400

    Updated config files

--- base/work/default.nix
+++ base/work/default.nix
@@ -132,40 +132,6 @@ in {
   };
 
   virtualisation.oci-containers.containers = {
-    npm = {
-      hostname = "npm";
-      image = "jc21/nginx-proxy-manager:latest";
-      ports = [
-        "127.0.0.1:80:80"
-        "127.0.0.1:443:443"
-        "127.0.0.1:81:81"
-      ];
-      volumes = [
-        "nginx-proxy-manager:/data"
-        "letsencrypt:/etc/letsencrypt"
-      ];
-      extraOptions = [
-        "--network=npm"
-      ];
-    };
-
-    mysql = {
-      hostname = "mysql";
-      image = "percona/percona-server:latest";
-      ports = [
-        "127.0.0.1:3306:3306"
-      ];
-      volumes = [
-        "percona-mysql:/var/lib/mysql"
-      ];
-      environment = {
-        MYSQL_ROOT_PASSWORD = "secret";
-      };
-      extraOptions = [
-        "--network=npm"
-      ];
-    };
-
     portainer = {
       hostname = "portainer";
       image = "portainer/portainer-ee:latest";
@@ -178,7 +144,105 @@ in {
         "/var/run/docker.sock:/var/run/docker.sock"
       ];
       extraOptions = [
-        "--network=npm"
+        "--network=local"
+      ];
+    };
+
+    nginx-proxy-manager = {
+      hostname = "npm";
+      image = "jc21/nginx-proxy-manager:latest";
+      ports = [
+        "127.0.0.1:80:80"
+        "127.0.0.1:443:443"
+        "127.0.0.1:81:81"
+      ];
+      volumes = [
+        "nginx-proxy-manager:/data"
+        "letsencrypt:/etc/letsencrypt"
+      ];
+      extraOptions = [
+        "--network=local"
+      ];
+    };
+
+    mysql = {
+      hostname = "mysql";
+      image = "percona/percona-server:latest";
+      ports = [
+        "127.0.0.1:3306:3306"
+      ];
+      volumes = [
+        "percona-mysql:/var/lib/mysql"
+      ];
+      extraOptions = [
+        "--network=local"
+      ];
+      environment = {
+        MYSQL_ROOT_PASSWORD = "secret";
+      };
+    };
+
+    redis = {
+      hostname = "redis";
+      image = "redis:alpine";
+      ports = [
+        "127.0.0.1:6379:6379"
+      ];
+      volumes = [
+        "redis:/data"
+      ];
+      extraOptions = [
+        "--network=local"
+      ];
+    };
+
+    meiliseach = {
+      hostname = "meiliseach";
+      image = "getmeili/meilisearch:latest";
+      ports = [
+        "127.0.0.1:7700:7700"
+      ];
+      volumes = [
+        "meiliseach:/meili_data"
+      ];
+      extraOptions = [
+        "--network=local"
+      ];
+      environment = {
+        MEILI_NO_ANALYTICS = "false";
+      };
+    };
+
+    soketi = {
+      hostname = "soketi";
+      image = "getmeili/meilisearch:latest";
+      ports = [
+        "127.0.0.1:6001:6001"
+        "127.0.0.1:9601:9601"
+      ];
+      extraOptions = [
+        "--network=local"
+      ];
+      environment = {
+        SOKETI_DEBUG = "1";
+        SOKETI_METRICS_SERVER_PORT = "9601";
+        SOKETI_DEFAULT_APP_ID = "soketi";
+        SOKETI_DEFAULT_APP_KEY = "soketi";
+        SOKETI_DEFAULT_APP_SECRET = "soketi";
+      };
+    };
+
+    buggregator = {
+      hostname = "buggregator";
+      image = "ghcr.io/buggregator/server:latest";
+      ports = [
+        "127.0.0.1:8000:8000"
+        "127.0.0.1:1025:1025"
+        "127.0.0.1:9912:9912"
+        "127.0.0.1:9913:9913"
+      ];
+      extraOptions = [
+        "--network=local"
       ];
     };
   };
