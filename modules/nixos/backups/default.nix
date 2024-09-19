e8ea1be82beb70c09623fbba02ed0dabdcd48a7c
Victor R <39545521+viicslen@users.noreply.github.com>
2024-09-19 09:49:31 -0400

    Updated config files

--- modules/nixos/backups/default.nix
+++ modules/nixos/backups/default.nix
@@ -120,13 +120,13 @@ in {
     environment.systemPackages = [ 
       (pkgs.writeShellScriptBin "notify-send" ''
         #Detect the name of the display in use
-        local display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
+        display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
 
         #Detect the user using such display
-        local user=$(who | grep '('$display')' | awk '{print $1}' | head -n 1)
+        user=$(who | grep '('$display')' | awk '{print $1}' | head -n 1)
 
         #Detect the id of the user
-        local uid=$(id -u $user)
+        uid=$(id -u $user)
 
         sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus ${pkgs.libnotify}/bin/notify-send "$@"
       '')
