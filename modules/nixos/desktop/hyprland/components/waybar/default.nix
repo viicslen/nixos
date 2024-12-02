3b92e783f17d7fe0d7f10e9cd4934c658d01b92a
Victor R <39545521+viicslen@users.noreply.github.com>
2024-12-02 00:02:48 -0500

    Refactor Waybar configuration: separate styles into a new CSS file and enable network and Bluetooth applets

--- modules/nixos/desktop/hyprland/components/waybar/default.nix
+++ modules/nixos/desktop/hyprland/components/waybar/default.nix
@@ -1,15 +1,25 @@
 {
   pkgs,
-  config,
-  lib,
   ...
 }: let
-  betterTransition = "all 0.3s cubic-bezier(.55,-0.68,.48,1.682)";
-  colorScheme = config.lib.stylix.colors;
   pipewireStatus = import ./scripts/pipewire.nix {inherit pkgs;};
   mullvadStatus = import ./scripts/mullvad.nix {inherit pkgs;};
-in
-  with lib; {
+in {
+    # Enable Network and Bluetooth Applet
+    services.network-manager-applet.enable = true;
+    services.blueman-applet.enable = true;
+
+    # Configure Hyprland
+    wayland.windowManager.hyprland.settings = {
+      exec-once = [
+        "killall -q waybar;sleep .5 && waybar"
+      ];
+
+      bind = [
+        # "$mod CTRL SHIFT, R, exec, killall -q waybar;sleep .5 && waybar"
+      ];
+    };
+
     # Configure & Theme Waybar
     programs.waybar = {
       enable = true;
@@ -182,134 +192,6 @@ in
           };
         }
       ];
-      style = let
-        workspaceRadius = "16px";
-        moduleBackground = "@base01";
-      in ''
-        * {
-          font-size: 14px;
-          border-radius: 0px;
-          border: none;
-          font-family: JetBrainsMono Nerd Font;
-          min-height: 0px;
-        }
-        window#waybar {
-          background-color: transparent;
-        }
-        window#waybar.empty #window {
-          background: none;
-          padding: 0px;
-        }
-        tooltip {
-          background: ${moduleBackground};
-          border: 1px solid @base0E;
-          border-radius: 12px;
-        }
-        tooltip label {
-          color: @base07;
-        }
-
-        #workspaces {
-          color: @base00;
-          background: ${moduleBackground};
-          margin: 4px 4px;
-          padding: 5px;
-          border-radius: ${workspaceRadius};
-        }
-        #workspaces button {
-          font-weight: bold;
-          padding: 0px 5px;
-          margin: 0px 3px;
-          border-radius: ${workspaceRadius};
-          color: @base00;
-          background: linear-gradient(45deg, @base0E, @base0F, @base0D, @base09);
-          background-size: 300% 300%;
-          opacity: 0.5;
-        }
-        #workspaces button.active {
-          opacity: 1.0;
-          min-width: 40px;
-          border-bottom: none;
-        }
-        #workspaces button:hover {
-          opacity: 0.8;
-        }
-        .modules-left #workspaces button {
-            border-bottom: 0 solid transparent;
-        }
-        .modules-left #workspaces button.focused,
-        .modules-left #workspaces button.active {
-            border-bottom: 0 solid @base05;
-        }
-        .modules-center #workspaces button {
-            border-bottom: 0 solid transparent;
-        }
-        .modules-center #workspaces button.focused,
-        .modules-center #workspaces button.active {
-            border-bottom: 0 solid @base05;
-        }
-        .modules-right #workspaces button {
-            border-bottom: 0 solid transparent;
-        }
-        .modules-right #workspaces button.focused,
-        .modules-right #workspaces button.active {
-            border-bottom: 0 solid @base05;
-        }
-
-        #custom-startmenu {
-          color: @base0D;
-          background: ${moduleBackground};
-          font-size: 24px;
-          margin: 0px;
-          padding: 0px 35px 0px 15px;
-          border-radius: 0px 0px 40px 0px;
-        }
-        #custom-exit {
-          font-weight: bold;
-          color: @base00;
-          background: linear-gradient(45deg, @base0C, @base0F, @base0B, @base08);
-          background-size: 300% 300%;
-          margin: 0px;
-          padding: 0px 15px 0px 30px;
-          border-radius: 0px 0px 0px 40px;
-        }
-
-        #window, #cpu, #memory, #idle_inhibitor, #custom-vpn {
-          font-weight: bold;
-          margin: 4px 0px;
-          margin-left: 7px;
-          padding: 0px 18px;
-          color: @base05;
-          background: ${moduleBackground};
-          border-radius: 24px 10px 24px 10px;
-        }
-        #network, #battery, #pulseaudio, #tray, #clock,
-        #custom-notification, #custom-pipewire, #wireplumber, #backlight {
-          font-weight: bold;
-          background: ${moduleBackground};
-          color: @base05;
-          margin: 4px 0px;
-          margin-right: 7px;
-          border-radius: 10px 24px 10px 24px;
-          padding: 0px 18px;
-        }
-      '';
-    };
-
-    # Enable Network and Bluetooth Applet
-    services.network-manager-applet.enable = true;
-    services.blueman.enable = true;
-
-    # Configure Hyprland
-    wayland.windowManager.hyprland.settings = {
-      "$launcher" = "${pkgs.rofi-wayland}/bin/rofi -show drun";
-
-      exec-once = [
-        "killall -q waybar;sleep .5 && waybar"
-      ];
-
-      bind = [
-        "$mod CTRL SHIFT, R, exec, killall -q waybar;sleep .5 && waybar"
-      ];
+      style = (builtins.unsafeDiscardStringContext (builtins.readFile ./style.css));
     };
   }
