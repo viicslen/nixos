59271501462593f490a421d4e4cc67d43b9cfbc2
Victor R <39545521+viicslen@users.noreply.github.com>
2024-12-18 01:11:04 -0500

    Refactor nushell configuration by removing unused plugin activation scripts and updating plugin list

--- modules/home-manager/programs/nushell/default.nix
+++ modules/home-manager/programs/nushell/default.nix
@@ -10,41 +10,9 @@ with lib; let
   namespace = "programs";
 
   cfg = config.modules.${namespace}.${name};
-
-  activateNushellPluginsNuScript = pluginNames: pkgs.writeTextFile {
-    name = "activateNushellPlugins";
-    destination = "/bin/activateNushellPlugins.nu";
-    text = ''
-      #!/usr/bin/env nu
-      ${
-        concatStringsSep "\n" (map (x: "plugin add ${pkgs.nushellPlugins.${x}}/bin/nu_plugin_${x}") pluginNames)
-      }
-    '';
-  };
-
-  msgPackz = pluginNames: pkgs.runCommand "nushellMsgPackz" {} ''
-    mkdir -p "$out"
-    # After some experimentation, I determined that this only works if --plugin-config is FIRST
-    ${pkgs.nushell}/bin/nu --plugin-config "$out/plugin.msgpackz" ${activateNushellPluginsNuScript pluginNames}/bin/activateNushellPlugins.nu
-  '';
 in {
   options.modules.${namespace}.${name} = {
     enable = mkEnableOption (mdDoc "nushell");
-
-    plugins = mkOption {
-      type = types.listOf types.str;
-      default = [
-        # "highlight"
-        # "formats"
-        # "units"
-        # "query"
-        # "gstat"
-        # "net"
-      ];
-      description = ''
-        A list of plugins to install for nushell.
-      '';
-    };
   };
 
   config = mkIf cfg.enable {
@@ -72,18 +40,28 @@ in {
 
           dep = "vendor/bin/dep";
           sail = "vendor/bin/sail";
-          s = "sail";
-          sud = "sail up -d";
-          sdown = "sail down";
-          art = "sail artisan";
-          sa = "sail artisan";
-          sc = "sail composer";
-          sp = "sail php";
-          sn = "sail npm";
-          st = "sail tinker";
-          sd = "sail debug";
-          sda = "sail debug artisan";
+          s = "vendor/bin/sail";
+          sud = "vendor/bin/sail up -d";
+          sdown = "vendor/bin/sail down";
+          art = "vendor/bin/sail artisan";
+          sa = "vendor/bin/sail artisan";
+          sc = "vendor/bin/sail composer";
+          sp = "vendor/bin/sail php";
+          sn = "vendor/bin/sail npm";
+          st = "vendor/bin/sail tinker";
+          sd = "vendor/bin/sail debug";
+          sda = "vendor/bin/sail debug artisan";
         };
+
+        plugins = with pkgs.nushellPlugins; [
+          net
+          dbus
+          units
+          query
+          qstat
+          formats
+          highlight
+        ];
 
         extraConfig = ''
           ${(builtins.unsafeDiscardStringContext (builtins.readFile ./config.nu))}
@@ -104,7 +82,5 @@ in {
       zoxide.enableNushellIntegration = true;
       yazi.enableNushellIntegration = true;
     };
-
-    xdg.configFile."nushell/plugin.msgpackz".source = "${msgPackz cfg.plugins}/plugin.msgpackz";
   };
 }
