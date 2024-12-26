599ed763952bb2747b357ffeeaafab1af26e2d17
Victor R <39545521+viicslen@users.noreply.github.com>
2024-12-26 10:20:33 -0500

    Add udev rules for VIA compatible keyboards and enable QMK support

--- modules/nixos/programs/qmk/default.nix
+++ modules/nixos/programs/qmk/default.nix
@@ -22,12 +22,13 @@ in {
       qmk_hid
     ];
 
+    services.udev.packages = [ pkgs.via ];
+
     hardware.keyboard.qmk.enable = true;
 
-    # services.udev.extraRules = mkAfter ''
-    #   # Configuration of VIA compatible keyboards
-    #   # https://wiki.archlinux.org/title/Keyboard_input#Configuration_of_VIA_compatible_keyboards
-    #   KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0666", TAG+="uaccess", TAG+="udev-acl"
-    # ''
+    services.udev.extraRules = mkAfter ''
+      # Configuration of VIA compatible keyboards
+      ${(builtins.unsafeDiscardStringContext (builtins.readFile ./qmk.rules))}
+    '';
   };
 }
