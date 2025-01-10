7763be12e1a05bbd139b6c05d1225e2d3a4e6fad
Victor R <39545521+viicslen@users.noreply.github.com>
2025-01-10 00:27:54 -0500

    Add configuration files for Hyprland shell components

--- modules/nixos/desktop/hyprland/components/shell/default.nix
+++ modules/nixos/desktop/hyprland/components/shell/default.nix
@@ -2,4 +2,24 @@
   home.packages = with pkgs; [
     inputs.shell.default
   ];
+
+  xdg.configFile."marble".source = ./config;
+
+  wayland.windowManager.hyprland.settings = let
+    shell = "marble -b hypr";
+  in {
+    "$launcher" = "${shell} -t launcher";
+
+    exec-once = [
+      "uwsm app -- ${shell}"
+    ];
+
+    bindr = [
+      "$mod, SUPER_L, exec, ${shell} -t launcher"
+    ];
+
+    bind = [
+      ",XF86PowerOff, exec, ${shell} -t 'powermenu'"
+    ];
+  };
 }
