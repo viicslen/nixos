499c44d8c7b0189b22a5e3f5aab3fc9869aa019b
Victor R <39545521+viicslen@users.noreply.github.com>
2025-02-27 11:23:51 -0500

    Update vimPlugins configuration for neotest-pest and add build inputs

--- pkgs/vimPlugins/default.nix
+++ pkgs/vimPlugins/default.nix
@@ -1,4 +1,4 @@
-{pkgs}: {
+{pkgs, ...}: rec {
   laravel-nvim = pkgs.callPackage ./laravel-nvim.nix {};
   neotest-pest = pkgs.callPackage ./neotest-pest.nix {};
 }
--- pkgs/vimPlugins/neotest-pest.nix
+++ pkgs/vimPlugins/neotest-pest.nix
@@ -3,8 +3,11 @@
     owner = "V13Axel";
     repo = "neotest-pest";
     rev = "v1.0";
-    sha256 = "";
+    sha256 = "sha256-8iCGpbrDnqJLTiB9oe5RvpTAgi9J9D0y7VzSw9qd0oQ=";
   };
   pname = "neotest-pest";
   version = "1.0";
+  buildInputs = with vimPlugins; [
+    neotest
+  ];
 }
