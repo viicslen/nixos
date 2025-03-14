6b49168deb1ffda76fcd4f96871a7ae6d2df1257
Victor R <39545521+viicslen@users.noreply.github.com>
2025-03-14 16:41:47 -0400

    Fix formatting in default.nix by removing unnecessary whitespace in inputsFrom

--- dev-shells/default.nix
+++ dev-shells/default.nix
@@ -6,7 +6,7 @@
   ...
 }: {
   default = pkgs.mkShell {
-    inputsFrom = [ config.mission-control.devShell ];
+    inputsFrom = [config.mission-control.devShell];
   };
   kubernetes = import ./kubernetes.nix {inherit inputs system;};
   laravel = import ./laravel.nix {inherit inputs system;};
