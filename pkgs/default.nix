b32b59f91e2fc642589f0d2b166d64c6ebb04004
Victor R <39545521+viicslen@users.noreply.github.com>
2025-04-29 09:05:20 -0400

    Add electron 46 pkg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,4 +7,5 @@
   myScripts = import ./scripts {inherit pkgs;};
   iso-image = import ./iso {inherit inputs pkgs;};
   vial = pkgs.callPackage ./by-name/vi/vial {};
+  electron_36-bin = pkgs.callPackage ./by-name/el/electron_36 {};
 }
