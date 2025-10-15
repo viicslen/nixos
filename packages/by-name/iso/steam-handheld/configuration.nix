39a5b392f7683666f5697a656628cc78b1598d74
Victor R <39545521+viicslen@users.noreply.github.com>
2025-10-14 21:07:01 -0400

    Lint config

--- packages/by-name/iso/steam-handheld/configuration.nix
+++ packages/by-name/iso/steam-handheld/configuration.nix
@@ -1,21 +1,26 @@
-{inputs, pkgs, lib, ...}: {
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     inputs.jovian.nixosModules.default
   ];
 
   hardware.amdgpu.initrd.enable = false;
-  boot.kernelParams = [ "quiet" ];
+  boot.kernelParams = ["quiet"];
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos;
   boot.kernel.sysctl = {
     "kernel.split_lock_mitigate" = 0;
-    "kernel.nmi_watchdog"        = 0;
-    "kernel.sched_bore"          = "1";
+    "kernel.nmi_watchdog" = 0;
+    "kernel.sched_bore" = "1";
   };
 
   boot.initrd = {
-    systemd.enable   = true;
-    kernelModules    = [ ];
-    verbose          = false;
+    systemd.enable = true;
+    kernelModules = [];
+    verbose = false;
   };
 
   jovian.hardware.has.amd.gpu = true;
