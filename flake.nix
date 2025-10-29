8096ab13516892aabdafa01d4917dd2ce5071012
Victor R <39545521+viicslen@users.noreply.github.com>
2025-10-28 23:28:38 -0400

    Add dgop, dms-cli, and dankMaterialShell modules with dependencies

--- flake.nix
+++ flake.nix
@@ -91,6 +91,20 @@
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    dgop = {
+      url = "github:AvengeMedia/dgop";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dms-cli = {
+      url = "github:AvengeMedia/danklinux";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dankMaterialShell = {
+      url = "github:AvengeMedia/DankMaterialShell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.dgop.follows = "dgop";
+      inputs.dms-cli.follows = "dms-cli";
+    };
 
     # Hyprland
     hyprland.url = "github:hyprwm/Hyprland";
