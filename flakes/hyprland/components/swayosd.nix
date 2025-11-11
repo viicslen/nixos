15217119b249b14a93343d09131bf48905418cb3
Victor R <39545521+viicslen@users.noreply.github.com>
2025-11-11 11:07:24 -0500

    Remove flakes/hyprland before converting to submodule

--- flakes/hyprland/components/swayosd.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  home.packages = [
-    pkgs.swayosd
-  ];
-
-  services.swayosd = {
-    enable = true;
-  };
-}
--- flakes/hyprland/components/workspaces/shell.nix
+++
@@ -1,6 +0,0 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  packages = [
-    (import ./work.nix {inherit pkgs;})
-  ];
-}
