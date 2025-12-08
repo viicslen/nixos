5801b9e409c6462da6b26935c059121f6af15586
Victor R <39545521+viicslen@users.noreply.github.com>
2025-12-08 17:19:45 -0500

    Remove flakes/nixvim before converting to submodule

--- flakes/nixvim/packages.nix
+++
@@ -1,7 +0,0 @@
-# Package outputs for the NixVim flake
-# This file is imported by flake.nix via flake-parts
-{
-  # Package outputs are defined in perSystem in flake.nix
-  # This file exists for structural consistency with other flakes
-  # and can be extended if more complex package definitions are needed
-}
